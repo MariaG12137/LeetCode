140d753ad75591d32c8ca0e3d4da98aa5b3bca93
Maria Guo <68859666+MariaG12137@users.noreply.github.com>
2020-08-23 15:28:58 -0400

    Create 876. Middle of the Linked List.js

---
+++ 876. Middle of the Linked List.js
@@ -0,0 +1,20 @@
+/*
+Given a non-empty, singly linked list with head node head, return a middle node of linked list.
+
+If there are two middle nodes, return the second middle node.
+*/
+var middleNode = function(head) {
+    if(!head)return [];
+    let p1 = head;
+    let p2 = head;
+    
+    while(p2.next !== null && p2.next.next !== null){
+        p1=p1.next;
+        p2=p2.next.next;
+    }
+    if(p2.next == null){
+        return p1;
+    }else{
+        return p1.next
+    }
+};

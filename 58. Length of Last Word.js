f22b08d9b99b02bd4e7bfeb5d01955bc7530de2b
Maria Guo <68859666+MariaG12137@users.noreply.github.com>
2020-09-16 17:12:38 -0400

    Update 58. Length of Last Word.js

--- 58. Length of Last Word.js
+++ 58. Length of Last Word.js
@@ -39,3 +39,18 @@ var lengthOfLastWord = function(s) {
     }
     return count;
 };
+=========================================================================================================================
+//second submission
+var lengthOfLastWord = function(s) {
+    let result = 0;
+    for(let i=s.length-1;i>=0;i--){
+        if(s[i]!==' '){
+            result++;
+        }else{
+            if(result !== 0){
+                break
+            }
+        }
+    }
+    return result
+};

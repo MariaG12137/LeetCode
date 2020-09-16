1b9c09b63c3c2220d0a4cc7684ca1f6d2a456ce5
Maria Guo <68859666+MariaG12137@users.noreply.github.com>
2020-09-16 17:50:38 -0400

    Update 66. Plus One.js

--- 66. Plus One.js
+++ 66. Plus One.js
@@ -22,3 +22,43 @@ var plusOne = function(digits) {
 
     return digits
 };
+=================================================================================================================
+var plusOne = function(digits) {
+    let len = digits.length-1;
+    let digit = 0;
+    
+    function plus(len,incre){
+        if(len == -1 && incre==1){
+            digits.unshift(1);
+            return;
+        }
+        digit = digits[len];
+        if(digit+incre>9){
+            digits[len]=digit+incre-10;
+            len--;
+            plus(len,1);
+        }else{
+            digits[len]=digit+incre;
+            return;
+        }
+    }
+    plus(len,1);
+    return digits
+};
+var plusOne = function(digits) {
+
+    let incre = 1;
+    for(let i=digits.length-1; i>=-1;i--){
+        if(i==-1){
+            digits.unshift(1);
+            break;
+        }
+        if(digits[i] + incre >9){
+            digits[i]=digits[i]+incre-10;
+        }else{
+            digits[i]=digits[i]+incre;
+            break;
+        }
+    }
+    return digits
+};

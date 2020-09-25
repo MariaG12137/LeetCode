cf01db45d2833bf4cfa34972414163cc0182acc6
Maria Guo <68859666+MariaG12137@users.noreply.github.com>
2020-09-24 23:50:40 -0400

    Update 349. Intersection of Two Arrays.js

--- 349. Intersection of Two Arrays.js
+++ 349. Intersection of Two Arrays.js
@@ -35,3 +35,27 @@ var intersection = function(nums1, nums2) {
     }
     return result;
 };
+====================================================================================================================
+//second submission
+var reverseVowels = function(s) {
+    let map = new Map();
+    s =s.split('');
+    map.set('a').set('e').set('i').set('o').set('u').set('A').set('E').set('I').set('O').set('U');
+    let l=0;
+    let r=s.length-1;
+    
+    while(l<r){
+        if(map.has(s[l])&& map.has(s[r])){
+            [s[l],s[r]]=[s[r],s[l]];
+            l++;
+            r--
+        }
+        if(!map.has(s[l])){
+            l++
+        }
+        if(!map.has(s[r])){
+            r--
+        } 
+    }
+    return s.join('')
+};

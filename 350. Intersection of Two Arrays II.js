63eed8b98a635be6d660a1e0f19d3229af0db8b3
Maria Guo <68859666+MariaG12137@users.noreply.github.com>
2020-09-25 00:09:06 -0400

    Update 350. Intersection of Two Arrays II.js

--- 350. Intersection of Two Arrays II.js
+++ 350. Intersection of Two Arrays II.js
@@ -23,3 +23,49 @@ var intersect = function(nums1, nums2) {
     console.log(map, result);
     return result;
 };
+======================================================================================================================
+//second submission
+var intersect = function(nums1, nums2) {
+    let map = new Map();
+    let result = [];
+    for(let num of nums1){
+        if(!map.has(num)){
+            map.set(num,[1,0])
+        }else{
+            map.get(num)[0]++
+        }
+    }
+    
+    for(let num of nums2){
+        if(map.has(num)){
+            map.get(num)[1]++
+        }
+    }
+    
+    for(let entrie of map.entries()){
+        for(let i=0; i<Math.min(entrie[1][0],entrie[1][1]);i++){
+            result.push(entrie[0])
+        }
+    }
+    return result;
+};
+============================================================================================================================
+var intersect = function(nums1, nums2) {
+    let map = new Map();
+    let result = [];
+    for(let num of nums1){
+        if(!map.has(num)){
+            map.set(num,[1])
+        }else{
+            map.get(num)[0]++
+        }
+    }
+    
+    for(let num of nums2){
+        if(map.has(num) && map.get(num)>0){
+            map.get(num)[0]--;
+            result.push(num)
+        }
+    }
+    return result;
+};

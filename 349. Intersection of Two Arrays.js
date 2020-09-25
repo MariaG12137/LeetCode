1533fe68aabba8e8c515fee3012fdbffaf1c2c70
Maria Guo <68859666+MariaG12137@users.noreply.github.com>
2020-09-24 23:51:13 -0400

    Update 349. Intersection of Two Arrays.js

--- 349. Intersection of Two Arrays.js
+++ 349. Intersection of Two Arrays.js
@@ -37,25 +37,15 @@ var intersection = function(nums1, nums2) {
 };
 ====================================================================================================================
 //second submission
-var reverseVowels = function(s) {
-    let map = new Map();
-    s =s.split('');
-    map.set('a').set('e').set('i').set('o').set('u').set('A').set('E').set('I').set('O').set('U');
-    let l=0;
-    let r=s.length-1;
+var intersection = function(nums1, nums2) {
+    let set1 = new Set(nums1);
+    let set2 = new Set(nums2);
+    let result = [];
     
-    while(l<r){
-        if(map.has(s[l])&& map.has(s[r])){
-            [s[l],s[r]]=[s[r],s[l]];
-            l++;
-            r--
+    for(let num of set1){
+        if(set2.has(num)){
+            result.push(num)
         }
-        if(!map.has(s[l])){
-            l++
-        }
-        if(!map.has(s[r])){
-            r--
-        } 
     }
-    return s.join('')
+    return result;
 };

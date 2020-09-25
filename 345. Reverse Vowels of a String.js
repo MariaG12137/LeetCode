76cf3349a1f62031c5fb94a45d5e784ae7855415
Maria Guo <68859666+MariaG12137@users.noreply.github.com>
2020-09-24 23:42:44 -0400

    Update 345. Reverse Vowels of a String.js

--- 345. Reverse Vowels of a String.js
+++ 345. Reverse Vowels of a String.js
@@ -22,3 +22,27 @@ var reverseVowels = function(s) {
     return s.join('');
     
 };
+=================================================================================================================
+//secound submission, using map
+var reverseVowels = function(s) {
+    let map = new Map();
+    s =s.split('');
+    map.set('a').set('e').set('i').set('o').set('u').set('A').set('E').set('I').set('O').set('U');
+    let l=0;
+    let r=s.length-1;
+    
+    while(l<r){
+        if(map.has(s[l])&& map.has(s[r])){
+            [s[l],s[r]]=[s[r],s[l]];
+            l++;
+            r--
+        }
+        if(!map.has(s[l])){
+            l++
+        }
+        if(!map.has(s[r])){
+            r--
+        } 
+    }
+    return s.join('')
+};

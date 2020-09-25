50ceede2d841bccb5f97ba081277fc3aee43677f
Maria Guo <68859666+MariaG12137@users.noreply.github.com>
2020-09-24 23:16:22 -0400

    Update 283. Move Zeroes.js

--- 283. Move Zeroes.js
+++ 283. Move Zeroes.js
@@ -31,3 +31,15 @@ var moveZeroes = function(nums) {
         }
     }
 };
+========================================================================================================
+//second submission
+var moveZeroes = function(nums) {
+    let p1 = 0;
+
+    for(let i=0; i<nums.length; i++){
+        if(nums[i]!==0){
+            [nums[p1],nums[i]] = [nums[i],nums[p1]];
+            p1++;
+        }
+    }
+};

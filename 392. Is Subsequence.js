1a510d3ee725d75c13efda9a4257a32ed46a3a77
Maria Guo <68859666+MariaG12137@users.noreply.github.com>
2020-09-25 21:19:41 -0400

    Update 392. Is Subsequence.js

--- 392. Is Subsequence.js
+++ 392. Is Subsequence.js
@@ -21,3 +21,18 @@ var isSubsequence = function(s, t) {
     return i==s.length
 };
 //for follow up, store t in a hash map
+=======================================================================================================================
+var isSubsequence = function(s, t) {
+    
+    let p1=0;
+    let p2=0;
+    while(s[p1]&&t[p2]){
+        if(s[p1] !== t[p2]){
+            p2++;
+        }else{
+            p1++;
+            p2++
+        }
+    }
+    return !s[p1]
+};

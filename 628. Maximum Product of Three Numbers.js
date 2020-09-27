f041aa5b2224a52151555f44f73ef3f9b01fa6d9
Maria Guo <68859666+MariaG12137@users.noreply.github.com>
2020-09-27 15:40:01 -0400

    Update 628. Maximum Product of Three Numbers.js

--- 628. Maximum Product of Three Numbers.js
+++ 628. Maximum Product of Three Numbers.js
@@ -6,3 +6,29 @@ var maximumProduct = function(nums) {
     return Math.max(nums[0]*nums[1]*nums[nums.length-1], nums[nums.length-1]*nums[nums.length-2]*nums[nums.length-3])
   
 };
+==========================================================================================================================
+var maximumProduct = function(nums) {
+    let min = [];
+    let max = [];
+    let array = [];
+    
+    function getMaxMin(array){ //constant time b/c size of array will be constant
+        array.sort((a,b)=>a-b);
+        min = array.slice(0,2);
+        array.sort((a,b)=>b-a);
+        max = array.slice(0,3) 
+    }
+    
+    for(let num of nums){
+        if(array.length<5){
+            array.push(num)
+        }else{
+            array.push(num);
+            getMaxMin(array);
+            array.splice(3,1)
+        }
+    }
+    getMaxMin(array);
+ 
+    return Math.max(min[0]*min[1]*max[0],max[0]*max[1]*max[2])
+};

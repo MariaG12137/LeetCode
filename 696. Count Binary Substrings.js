8a4e03382b0b43c9820c96fd1df69b50c500794b
Maria Guo <68859666+MariaG12137@users.noreply.github.com>
2020-09-28 17:18:45 -0400

    Update 696. Count Binary Substrings.js

--- 696. Count Binary Substrings.js
+++ 696. Count Binary Substrings.js
@@ -75,3 +75,22 @@ var countBinarySubstrings = function(s) {
     }
     return count
 };
+======================================================================================================================
+var countBinarySubstrings = function(s) {
+    let prev = 0;
+    let curr = 1;
+    let ans = 0;
+    
+    for(let i=1; i<s.length;i++){
+        if(s[i]===s[i-1]){
+            curr++;
+        }else{
+            ans += Math.min(prev,curr);
+            prev = curr;
+            curr = 1;
+        }
+    }
+    ans += Math.min(prev,curr);
+    
+    return ans;
+}

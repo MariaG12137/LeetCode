3e81ee22a4cda51adc6936c606f542ad498d16b6
Maria Guo <68859666+MariaG12137@users.noreply.github.com>
2020-09-29 15:19:28 -0400

    Update 872. Leaf-Similar Trees.js

--- 872. Leaf-Similar Trees.js
+++ 872. Leaf-Similar Trees.js
@@ -17,3 +17,17 @@ var leafSimilar = function(root1, root2) {
     
     return leaf1.join(' ') === leaf2.join(' ')
 };
+=============================================================================================
+//second submission
+var leafSimilar = function(root1, root2) {
+    function traverse(node){
+        if(!node){
+            return [];
+        }
+        if(!node.left && !node.right){
+            return [node.val];
+        }
+        return traverse(node.left).concat(traverse(node.right));
+    }
+    return traverse(root1).join(' ')===traverse(root2).join(' ')
+};
